437521f1d84be712aebb35d8cae6680e83546fcc
kunjacko12 <kunjacko9999@gmail.com>
2026-02-07 11:32:02 +0530

    Update urls.py

--- tims/tims/users/urls.py
+++ tims/tims/users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import *
 app_name="users"
 
-<<<<<<< HEAD
 
 
 
@@ -12,20 +11,12 @@ from .views import user_update_view
 
 app_name = "users"
 urlpatterns = [
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
-    path( "my-progress/", views.my_progress, name="my_progress"),
-
-=======
-urlpatterns = [
-
     #path("~redirect/", view=user_redirect_view, name="redirect"),
     #path("~update/", view=user_update_view, name="update"),
     #path("<str:username>/", view=user_detail_view, name="detail"),
     path("register/", UserRegisterView.as_view(), name="user_register"),
     path("role/add/", RoleCreateView.as_view(), name="role_add"),
->>>>>>> akhil/pages
+      path( "my-progress/", views.my_progress, name="my_progress"),
 ]
 
 
