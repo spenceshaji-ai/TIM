0832418a2c9edeb82c8f47b1c76a32cbc714932e
spenceshaji-ai <spenceshaji@gmail.com>
2026-02-23 14:32:54 +0530

    ok

--- tims/config/settings/base.py
+++ tims/config/settings/base.py
@@ -112,7 +112,7 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "faculty:faculty_batch_course_students"
+LOGIN_REDIRECT_URL = "users:redirect"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "account_login"
 
