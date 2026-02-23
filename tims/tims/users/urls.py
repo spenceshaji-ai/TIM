0952003f6d18d4b5fcb7423e349c3736efa57c22
kunjacko12 <kunjacko9999@gmail.com>
2026-02-23 15:52:06 +0530

    courseses

--- tims/tims/users/urls.py
+++ tims/tims/users/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-from .views import(
-    UserRegisterView,
-    RoleCreateView,
-    LoginView
-    ) 
+from .views import *
 from .views import user_detail_view
 from .views import user_redirect_view
 from .views import user_update_view
