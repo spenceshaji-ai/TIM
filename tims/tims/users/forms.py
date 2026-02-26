f9d99e849ac65fce173180f59acdc05619952d4a
spenceshaji-ai <spenceshaji@gmail.com>
2026-02-26 14:16:40 +0530

    Update forms.py

--- tims/tims/users/forms.py
+++ tims/tims/users/forms.py
@@ -1,9 +1,3 @@
-from allauth.account.forms import SignupForm
-from allauth.socialaccount.forms import SignupForm as SocialSignupForm
-from django.contrib.auth import forms as admin_forms
-from django.utils.translation import gettext_lazy as _
-from django import forms
-from .models import User
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
